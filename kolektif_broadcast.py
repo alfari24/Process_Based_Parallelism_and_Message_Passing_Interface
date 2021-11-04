6d75fff46ee48a29f42fac274ecc3e803d602ec7
alfari_Zero <muhammadarialghifari57@gmail.com>
2021-11-04 15:26:28 +0700

    main

---
+++ kolektif_broadcast.py
@@ -0,0 +1,10 @@
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+if rank == 0:
+    variable_to_share = 100
+else :
+    variable_to_share = None
+
+variable_to_share = comm.bcast(variable_to_share, root = 0)
+print("proses = %d " %rank + "variable dibagikan = %d " %variable_to_share)
